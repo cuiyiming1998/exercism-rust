ca9835c37fb90bb3d1eae4c17e2e7b302823f76d
cuiyiming1998 <1269567664@qq.com>
2023-06-29 19:31:17 +0800

    feat: anagram refactor

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,21 +1,22 @@
 use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
-    let mut set = HashSet::new();
+    let lowered_word = word.to_lowercase();
+    let sorted_word = sorted_str(&lowered_word);
 
-    for &possible_anagram in possible_anagrams {
-        if word.to_lowercase() != possible_anagram.to_lowercase()
-            && sorted_str(word) == sorted_str(possible_anagram)
-        {
-            set.insert(possible_anagram);
-        }
-    }
-
-    set
+    possible_anagrams
+        .iter()
+        .filter(|&&possible_anagram| {
+            let lowered_possible_anagram = possible_anagram.to_lowercase();
+            lowered_word != lowered_possible_anagram
+                && sorted_word == sorted_str(&lowered_possible_anagram)
+        })
+        .cloned()
+        .collect()
 }
 
 fn sorted_str(word: &str) -> String {
-    let mut word: Vec<char> = word.to_lowercase().chars().collect();
+    let mut word: Vec<char> = word.chars().collect();
     word.sort();
 
     word.into_iter().collect()
